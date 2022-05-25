82f160c8bc6a3ffb8e48424267b531e0f7e99556
Game Lab <badidea2004@gmail.com>
2022-05-25 15:10:30 -0700

    Done! (Ish)

--- GameLabGame/Assets/Scripts/EndingStuff.cs
+++ GameLabGame/Assets/Scripts/EndingStuff.cs
@@ -19,6 +19,8 @@ public class EndingStuff : MonoBehaviour
     public GameObject polaroid;
     private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
     private const string TWEET_LANGUAGE = "en";
+    public GameObject bigimage;
+    public RawImage bigimage2;
 
     // Start is called before the first frame update
     void Start()
@@ -32,16 +34,11 @@ public class EndingStuff : MonoBehaviour
     }
     IEnumerator soundend()
     {
-        yield return new WaitForSeconds(ads.clip.length - 2);
+        yield return new WaitForSeconds(ads.clip.length-8f);
         float val2;
         for (val2 = 1; val2 > 0; val2 -= Time.deltaTime/2)
         {
             black.color = new Color(0, 0, 0, val2);
-            if (doneplayingendcutscene)
-            {
-                black.color = new Color(0,0,0,0);
-                StopCoroutine(soundend());
-            }
             yield return new WaitForEndOfFrame();
         }
         black.color = new Color(0,0,0,0);
@@ -51,9 +48,41 @@ public class EndingStuff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !doneplayingendcutscene)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             doneplayingendcutscene = true;
+            StopCoroutine(soundend());
+            black.color = new Color(0,0,0,0);
+            ads.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            selected = -1;
+            for (int x = 0; x < buttons.Length; x++)
+            {
+                buttons[x].GetComponent<Image>().color = Color.white;
+            }
+        }
+
+        if (selected == -1)
+        {
+            bigimage.SetActive(false);
+        }
+        else
+        {
+            bigimage.SetActive(true);
+            bigimage2.texture = textures[selected];
+        }
+    }
+
+    public void back()
+    {
+        selected = -1;
+        
+        for (int x = 0; x < buttons.Length; x++)
+        {
+            buttons[x].GetComponent<Image>().color = Color.white;
         }
     }
 
@@ -87,18 +116,19 @@ public class EndingStuff : MonoBehaviour
 
     public void tweet()
     {
-        if (selected == null) return; 
+        if (selected == -1) return; 
         var Bytes = textures[selected].EncodeToPNG();
     }
 
     public void delete(int i)
     {
-        if (selected == null) return;
+        if (selected == -1) return;
         Destroy(buttons[selected]);
+        selected = -1;
     }
     public void Save()
     {
-        if (selected == null) return;
+        if (selected == -1) return;
         var Bytes = textures[selected].EncodeToPNG();
         string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) +"/";
         path = path.Replace('\\','/');
@@ -106,5 +136,6 @@ public class EndingStuff : MonoBehaviour
         while (File.Exists(path + "Photo" + FileCounter + ".png"))
             FileCounter++;
         File.WriteAllBytes( path + "Photo" + FileCounter + ".png", Bytes);
+        selected = -1;
     }
 }
